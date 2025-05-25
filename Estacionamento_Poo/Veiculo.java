74f214686a851e4dfb035af7a981addeea484d71
Hugo G.Fernandes <hugocodingdev@gmail.com>
2025-05-25 17:16:25 -0300

    Adicionado

---
+++ Estacionamento_Poo/Veiculo.java
@@ -0,0 +1,40 @@
+package Estacionamento_Poo;
+
+import java.time.LocalDateTime;
+
+public class Veiculo {
+
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String tipo; // Pode ser "pequeno", "grande" ou "moto"
+    private LocalDateTime horaEntrada;
+
+    public Veiculo(String placa, String marca, String modelo, String tipo) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+        this.horaEntrada = LocalDateTime.now(); // A hora atual como entrada
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public LocalDateTime getHoraEntrada() {
+        return horaEntrada;
+    }
+}
